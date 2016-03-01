39c426492e8d0dbc444f0fa0be65c8d43ac27279
Michael Peterson <mikebean233@yahoo.com>
2016-02-29 22:20:09 -0800

    typos...

--- do_all.sh
+++ do_all.sh
@@ -14,16 +14,16 @@ echo "---------- REMOVING OLD OUTPUT FILES FROM HDFS-------------"
 sh remove_output_files.sh $hdfsProgramRootPath
 
 echo;echo
-echo "----------- COPYING NEW INPUT FILES TO HDFD ---------------"
+echo "----------- COPYING NEW INPUT FILES TO HDFS ---------------"
 sh copy_input_files.sh $hdfsProgramRootPath
 
 echo;echo
-echo "--------- EXECUTING PROGRAM ("world" as pattern) ------------"
+echo "--------- EXECUTING PROGRAM ("\"world\"" as pattern) ------------"
 sh run.sh $hdfsProgramRootPath "world"
 
 echo;echo
 echo "-------- COPYING OUTPUT FILES FROM HDFS TO LOCAL FS --------"
-sh copy_output_files.sh
+sh copy_output_files.sh $hdfsProgramRootPath
 
 echo;echo
 echo "-------------- PRINTING CONTENTS OF OUTPUT FILES -----------"
@@ -37,16 +37,16 @@ echo "---------- REMOVING OLD OUTPUT FILES FROM HDFS-------------"
 sh remove_output_files.sh $hdfsProgramRootPath
 
 echo;echo
-echo "----------- COPYING NEW INPUT FILES TO HDFD ---------------"
+echo "----------- COPYING NEW INPUT FILES TO HDFS ---------------"
 sh copy_input_files.sh $hdfsProgramRootPath
 
 echo;echo
-echo "--------- EXECUTING PROGRAM ("luck" as pattern) ------------"
+echo "--------- EXECUTING PROGRAM ("\"luck\"" as pattern) ------------"
 sh run.sh $hdfsProgramRootPath "luck"
 
 echo;echo
 echo "-------- COPYING OUTPUT FILES FROM HDFS TO LOCAL FS --------"
-sh copy_output_files.sh
+sh copy_output_files.sh $hdfsProgramRootPath
 
 echo;echo
 echo "-------------- PRINTING CONTENTS OF OUTPUT FILES -----------"
