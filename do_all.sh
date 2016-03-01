6cc9f7058d668c95a9898d3c9069dc6d9fc23cb9
Michael Peterson <mikebean233@yahoo.com>
2016-02-29 22:02:57 -0800

    "world" and "luck" as patterns, also starting arguments for scripts

--- do_all.sh
+++ do_all.sh
@@ -1,16 +1,31 @@
 #! /bin/bash
+
+hdfsUserPath=$1
+
+echo "path: $hdfsUserPath"
+
 echo;echo
 echo "---------- REMOVING OLD OUTPUT FILES FROM HDFS-------------"
-sh remove_output_files.sh
+sh remove_output_files.sh $hdfsUserPath
 echo;echo
 echo "----------- COPYING NEW INPUT FILES TO HDFD ---------------"
-sh copy_input_files.sh
+sh copy_input_files.sh $hdfsUserPath
 echo;echo
 echo "----------------- BUILDING PROGRAM -------------------------"
-sh build.sh
+sh build.sh $hdfsUserPath
 echo;echo
-echo "-------------------- EXECUTING PROGRAM ---------------------"
-sh run.sh
+
+echo "--------- EXECUTING PROGRAM ("world" as pattern) ------------"
+sh run.sh $hdfsUserPath "world"
+echo;echo
+echo "-------- COPYING OUTPUT FILES FROM HDFS TO LOCAL FS --------"
+sh copy_output_files.sh
+echo;echo
+echo "-------------- PRINTING CONTENTS OF OUTPUT FILES -----------"
+cat ./output/new/*
+
+echo "--------- EXECUTING PROGRAM ("luck" as pattern) ------------"
+sh run.sh $hdfsUserPath "luck"
 echo;echo
 echo "-------- COPYING OUTPUT FILES FROM HDFS TO LOCAL FS --------"
 sh copy_output_files.sh
