4e32e0d8e5cf8128033ac5a3893bb9a39fadfaa3
Michael Peterson <mikebean233@yahoo.com>
2016-02-29 20:37:12 -0800

    Script improvements

--- copy_output_files.sh
+++ copy_output_files.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
+rm -r ./output/new
 hadoop fs -copyToLocal /user/mpeterson10/wc/output/* ./output
--- do_all.sh
+++ do_all.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
-
+echo;echo
+echo "---------- REMOVING OLD OUTPUT FILES FROM HDFS-------------"
 sh remove_output_files.sh
+echo;echo
+echo "----------- COPYING NEW INPUT FILES TO HDFD ---------------"
 sh copy_input_files.sh
+echo;echo
+echo "----------------- BUILDING PROGRAM -------------------------"
 sh build.sh
+echo;echo
+echo "-------------------- EXECUTING PROGRAM ---------------------"
 sh run.sh
+echo;echo
+echo "-------- COPYING OUTPUT FILES FROM HDFS TO LOCAL FS --------"
 sh copy_output_files.sh
+echo;echo
+echo "-------------- PRINTING CONTENTS OF OUTPUT FILES -----------"
+cat ./output/new
+
 
